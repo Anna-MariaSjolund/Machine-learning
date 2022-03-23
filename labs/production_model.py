1abc571248c661baba939e54a076d302e9ca80ae
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2022-03-23 23:29:54 +0100

    Added code

--- labs/production_model.py
+++ labs/production_model.py
@@ -1,13 +1,14 @@
-#Import
+#Import packages
 import joblib
 import pandas as pd
 
-#Load Data and Model
-test_data = pd.read_csv("Data/test_samples.csv", sep=";")
+#Load data and model
+test_data = pd.read_csv("Data/test_samples.csv")
 cardio_predictor = joblib.load("Model/cardio_predictor")
 
 #Predict values
-predicted_values_for_deployed = cardio_predictor.predict(test_data)
+results = pd.DataFrame(cardio_predictor.predict_proba(test_data), columns=["probability class 0", "probability class 1"])
+results["prediction"] = cardio_predictor.predict(test_data)
 
-predictions = pd.DataFrame(columns=["Probability class 1", "Probability class 2", "Prediction"])
-
+#Save the predicted values
+results.to_csv("Data/prediction.csv", index=False)
